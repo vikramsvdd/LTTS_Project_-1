749a01528e0c0c567ef58440291ed207ab65d276
vikramsvdd <51330665+vikramsvdd@users.noreply.github.com>
2021-04-13 22:43:48 +0530

    Add files via upload

---
+++ Implementation/src/c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include "a.h"
+
+void drawBoard(){
+	system("cls");
+	printf("\n\n\t Tic Tac Toe \n\n");
+	printf("Player1 (X) - Player2 (O) \n\n\n");
+	printf("     |     |     \n");
+	printf("  %c  |  %c  |  %c  \n",square[1],square[2],square[3]);
+	printf("_____|_____|_____\n");
+	printf("     |     |     \n");
+	printf("  %c  |  %c  |  %c  \n",square[4],square[5],square[6]);
+	printf("_____|_____|_____\n");
+	printf("     |     |     \n");
+	printf("  %c  |  %c  |  %c  \n",square[7],square[8],square[9]);
+	printf("     |     |     \n");	
+}
